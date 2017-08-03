9dde5d9431821ce165d661ced56a83f9f594ea61
ligobird <335371349@qq.com>
2017-08-04 07:00:21 +0800

    first commit

---
+++ common/config/fecshop_local_services/Image.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * FecShop file.
+ * @link http://www.fecshop.com/
+ * @copyright Copyright (c) 2016 FecShop Software LLC
+ * @license http://www.fecshop.com/license/
+ */
+return [
+	'image' => [
+		'appbase'	=> [
+			'appfront' => [
+				'basedir' => '@appimage/appfront',
+				'basedomain' => 'http://img3.fecshoptest.com',
+			],
+			'apphtml5' => [
+				'basedir' => '@appimage/apphtml5',
+				'basedomain' => 'http://img2.fecshoptest.com',
+			],
+			'appadmin' => [
+				'basedir' => '@appimage/appadmin',
+				'basedomain' => 'http://img2.fecshoptest.com',
+			],
+			'common' => [
+				'basedir' => '@appimage/common',
+				'basedomain' => 'http://img.fecshoptest.com',
+			],
+		],
+	],
+];
